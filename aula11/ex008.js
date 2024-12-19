5976b865c976eb863076edc80ebefa7c833a3d0b
Fabio Nicola <90237498+fabionicola@users.noreply.github.com>
2024-12-19 17:55:26 -0300

    javaScript Curso em Video

---
+++ aula11/ex008.js
@@ -0,0 +1,13 @@
+console.log('O console funcionou')
+
+var vel = 12
+
+console.log(`A velocidade do carro é ${vel}`)
+
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)   
+} else {
+    
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
+
